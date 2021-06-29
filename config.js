5da8b53dd1fafedea25eda23999b7ab897f87cea
Lenovo <wridho246@gmail.com>
2021-06-29 13:41:15 +0700

    add config

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports ={
+    // MONGODB: 'mongodb+srv://Ridho:xynS85FkLWiUaWY@graphqlreactnodejs.nwusd.mongodb.net/merngql?authSource=admin&compressors=zlib&retryWrites=true&w=majority&ssl=true'
+    // MONGODB: 'mongodb+srv://nakivecode:XBsp9JmJN9EyOV2x@graphqlreactnode.m64le.mongodb.net/merngql?authSource=admin&replicaSet=atlas-t7w9h6-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true'
+    MONGODB: 'mongodb://nakivecode:XBsp9JmJN9EyOV2x@graphqlreactnode-shard-00-00.m64le.mongodb.net:27017,graphqlreactnode-shard-00-01.m64le.mongodb.net:27017,graphqlreactnode-shard-00-02.m64le.mongodb.net:27017/merng?ssl=true&replicaSet=atlas-t7w9h6-shard-0&authSource=admin&retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+
+};
+
