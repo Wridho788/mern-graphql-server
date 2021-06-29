836ae4189524929cb261df2dbaac669bd4175325
Lenovo <wridho246@gmail.com>
2021-06-29 13:09:00 +0700

    graphql react node verse freecodecamp.org

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,23 @@
+const postsResolvers = require('./posts');
+const usersResolvers = require('./user');
+const commentsResolvers = require('./comments');
+
+module.exports = { 
+    Post: {
+      likeCount: (parent) => parent.likes.length,
+      commentCount: (parent) => parent.comments.length
+    },
+    Query: {
+        ...postsResolvers.Query
+      },
+      Mutation: {
+        ...usersResolvers.Mutation,
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
+
+      },
+      Subscription: {
+        ...postsResolvers.Subscription
+      }
+
+};
